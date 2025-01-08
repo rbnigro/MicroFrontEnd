8d9e4cafa4462faef388b413c2f2b0986d1bdcaf
rbnigro <ronneynigro@gmail.com>
2025-01-08 11:38:44 -0300

    Shadow DOM

--- src/app/card/card.component.ts
+++ src/app/card/card.component.ts
@@ -1,11 +1,12 @@
-import { Component } from '@angular/core';
+import { Component, ViewEncapsulation } from '@angular/core';
 
 @Component({
   selector: 'app-card',
   standalone: false,
- 
+
   templateUrl: './card.component.html',
-  styleUrl: './card.component.scss'
+  styleUrl: './card.component.scss',
+  encapsulation: ViewEncapsulation.ShadowDom, // isola o componente atual não deixando que alterações globais o afetem
 })
 export class CardComponent {
    plano: any = {
